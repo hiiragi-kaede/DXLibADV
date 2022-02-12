fb526b2000b5999e57e921f956cdbe514f4fd1f7
hiiragi-kaede <zoro18782ryt131@gmail.com>
2022-02-12 14:06:39 +0900

    json読み込みのお試し

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,4 +1,10 @@
 #include "DxLib.h"
+#include<fstream>
+#include<string>
+#include"rapidjson/document.h"
+#include"rapidjson/istreamwrapper.h"
+
+using namespace rapidjson;
 
 const int TitleHandle = CreateFontToHandle(NULL, 50, 2);
 const int ScenarioSelectFontSize = 30;
@@ -23,7 +29,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
+	std::ifstream ifs("test_data/test.json");
+	IStreamWrapper isw(ifs);
+	Document doc;
+	doc.ParseStream(isw);
+
+	SetDrawScreen(DX_SCREEN_BACK);
+
 	while (true) {
+		ClearDrawScreen();
 		DrawString(100, 100, TitleText, color_white, TitleHandle);
 		for (int i = 0; i < 3; i++) {
 			if (i == ScenarioSelectNum) {
@@ -32,6 +46,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			else {
 				DrawString(100, 150 + ScenarioSelectFontSize * i, ScenariosTexts[i], color_white, ScenarioSelectHandle);
 			}
+		}
+
+		int cnt = 0;
+		for (auto itr = doc["test-data"].Begin(); itr != doc["test-data"].End(); itr++) {
+			std::string text{ (*itr)["name"].GetString()};
+			text += "  score:";
+			text += std::to_string((*itr)["test-result"].GetInt());
+			DrawString(100, 250 + cnt * 20, text.c_str(), color_white, ScenarioSelectHandle);
+			cnt++;
 		}
 
 		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
@@ -48,6 +71,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 				StartTime = GetNowCount();
 			}
 		}
+
+		ScreenFlip();
 	}
 
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
