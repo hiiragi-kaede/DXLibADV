245b922481aa241ac6e04b9ea65b7e6898582f75
hiiragi-kaede <zoro18782ryt131@gmail.com>
2022-03-10 09:15:41 +0900

    fps表示を仮実装

--- SceneManager.cpp
+++ SceneManager.cpp
@@ -2,6 +2,8 @@
 #include "SceneManager.hpp"
 #include"Game.hpp"
 #include"Title.hpp"
+#include"DxLibColor.hpp"
+#include<string>
 
 static eScene mScene = eScene_Title; // 現シーンの管理変数
 static eScene mNextScene = eScene_None; //次のシーン管理変数
@@ -9,9 +11,15 @@ static eScene mNextScene = eScene_None; //次のシーン管理変数
 static void SceneMgr_InitializeModule(eScene scene);//指定モジュールを初期化する
 static void SceneMgr_FinalizeModule(eScene scene);//指定モジュールの終了処理を行う
 
+static int FPSHandle;
+const int FPSFontSize = 30;
+static int start_cnt = 0;
+
 //初期化
 void SceneMgr_Initialize() {
     SceneMgr_InitializeModule(mScene);
+    start_cnt = GetNowCount();
+    FPSHandle = CreateFontToHandle(NULL, FPSFontSize, 2);
 }
 
 //シーンの更新
@@ -21,6 +29,8 @@ void SceneMgr_Update() {
         mScene = mNextScene;    //次のシーンを現在のシーンセット
         mNextScene = eScene_None;    //次のシーン情報をクリア
         SceneMgr_InitializeModule(mScene);    //現在のシーンを初期化
+
+        start_cnt = GetNowCount();
     }
 
     //現在のシーンによって処理を分岐
@@ -49,6 +59,12 @@ void SceneMgr_Draw() {
     default:
         break;
     }
+
+    int end_cnt = GetNowCount();
+    double fps = 1000.0 / (end_cnt - start_cnt);
+    start_cnt = end_cnt;
+    std::string time_text = std::to_string(fps) + "(fps)";
+    DrawStringToHandle(0, 0, time_text.c_str(), color_orange, FPSHandle);
 }
 
 //引数 nextScene のシーンを変更する
@@ -59,6 +75,7 @@ void SceneMgr_ChangeScene(eScene NextScene) {
 //終了処理
 void SceneMgr_Finalize() {
     SceneMgr_FinalizeModule(mScene);
+    DeleteFontToHandle(FPSHandle);
 }
 
 // 引数sceneモジュールを初期化する
